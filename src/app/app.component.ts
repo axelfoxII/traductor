ce0ffab18a14e0ec911b0fec82a52a585a07edef
Alex Salvatierra <asfoxii@gmail.com>
2025-09-27 22:20:16 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'traducirpage';
+  constructor(private translate: TranslateService) {
+    // Establece el idioma por defecto
+    translate.setDefaultLang('en');
+    translate.use('en'); // Idioma inicial
+  }
+
+  // Método para cambiar idioma
+  switchLanguage(lang: string) {
+    this.translate.use(lang);
+  }
+
+  mensaje(){
+    Swal.fire({
+      title: this.translate.instant('swal_title'),
+      text: this.translate.instant('swal_text'),
+      icon: 'success',
+
+    })
+  }  
 }
